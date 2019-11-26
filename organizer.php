50e670ae7579c2835bca59fb62ae61724d84eb52
ACM SIGMOD Workshop on Managing and Mining Enriched Geo-Spatial Data <58236368+georich2020@users.noreply.github.com>
2019-11-27 00:37:34 +0100

    Add files via upload

---
+++ organizer.php
@@ -0,0 +1,210 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<!--
+
+	Reverse Obscurity by nodethirtythree + Templated.org
+	http://templated.org/ | @templatedorg
+	Released under the Creative Commons Attribution 3.0 License.
+	
+	Note from the author: These templates take quite a bit of time to conceive,
+	design, and finally code. So please, support our efforts by respecting our
+	license: keep our footer credit links intact so people can find out about us
+	and what we do. It's the right thing to do, and we'll love you for it :)
+	
+-->
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head>
+        <meta name="keywords" content="georich, GeoRich 2016" />
+        <meta name="description" content="Third International ACM SIGMOD Workshop on Managing and Mining Semantically Rich Geo-Spatial Data" />
+        <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<title>GeoRich 2016 | Third International ACM SIGMOD Workshop on Managing and Mining Enriched Geo-Spatial Data</title>
+        <link href="http://fonts.googleapis.com/css?family=PT+Sans+Narrow" rel="stylesheet" type="text/css" />
+        <link href="http://fonts.googleapis.com/css?family=Lobster" rel="stylesheet" type="text/css" />
+		<link rel="stylesheet" type="text/css" href="style.css" />
+		
+
+    </head>
+    <body>
+
+		<div id="bg">
+			<div id="outer">
+			
+				<div id="banner">
+				<table cellspacing="0" cellpadding="0">
+					<tr>
+					<td valign="top" width="268px">&nbsp;&nbsp;<a href="/georich16/"><img src="./images/logo_small.png" height="110"></a></img></td>
+					<td width="1000px"><h1>Third International ACM SIGMOD Workshop on<br />Managing and Mining Enriched Geo-Spatial Data</h1><br><h2>GeoRich 2016 <small><small>Co-located with SIGMOD/PODS 2016 in San Francisco, CA, USA</small></small></h2></td>
+					<td> <a href="http://www.sigmod2016.org"><img src="./images/logo_sigmod.jpg"> </img></a></td>
+					<td> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </img></td>
+					</tr>
+				</table>
+				</div>
+				<div id="main">
+					<div id="sidebar1">
+						<h3>
+	Navigation
+</h3>
+<ul style="border-top:dotted 1px #bdbdbd;padding-top:8px;">
+	<li>
+		<a href="/georich16/">Home</a>
+	</li>
+	<li>
+		<a href="cfp.php">Call for Papers</a>
+	</li>
+	<!--<li>
+		<a href="accepted.php">Accepted Papers</a>
+	</li>
+	<li>
+		<a href="registration.php">Registration</a>
+	</li>-->
+	<li>
+		<a href="guidelines.php">Submission Guidelines</a>
+	</li>
+	<!--<li>
+		<a href="camera_ready.php">Camera-Ready Instructions</a>
+	</li>-->
+	<li>
+		<a href="program.php">Workshop Program</a>
+	</li>
+	<li>
+		<a href="organizer.php">Organizing Committee</a>
+	</li>
+</ul>
+
+<ul>
+	<li>
+
+		<a href="http://www.dbs.ifi.lmu.de/georich15/">GeoRich 2015</a>
+
+	</li>
+	<li>
+		<a href="http://www.dbs.ifi.lmu.de/georich14/">GeoRich 2014</a>
+	</li>
+</ul>
+					</div>
+					<div id="sidebarTwitter">
+							<a class="twitter-timeline"  href="https://twitter.com/search?q=%23georich16%20from%3AGeoRichWorkshop" data-widget-id="572698716914855936">Tweets about #georich16 from:GeoRichWorkshop</a>
+<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
+					</div>
+					<div id="sidebar2">
+						<h3>
+	Important Dates
+</h3>
+<ul class="linkedList">
+	<li>
+		<b>Workshop</b> <br>Sunday, 26.06.2016
+	</li>
+	<li>
+		<b>Submission Deadlines</b>
+		<table>
+			<tr>
+				<td>Abstract:</td>
+				<td><b>01.04.2016</b></td>
+			</tr>
+			<tr>
+				<td>Submission:</td>
+				<td><b>01.04.2016</b></td>
+			</tr>
+			<tr>
+				<td>Notification:</td>
+				<td><b>01.05.2016</b></td>
+			</tr>
+			<tr>
+				<td>Camera-Ready:</td>
+				<td><b>03.06.2016</b></td>
+			</tr>
+		</table>
+	<li>
+	<center>All Deadlines 11:59pm PST</center>
+	</li>
+</ul>
+
+					</div>
+					<div id="content">
+							<h3>Steering Committee</h3>
+							 Christian S. Jensen <i>(Aalborg University, Denmark)</i><br>
+							 Nikos Mamoulis <i>(The University of Hong Kong, China)</i><br>
+							 Kyriakos Mouratidis <i>(Singapore Management University, Singapore)</i><br>
+							 Mario A. Nascimento <i>(University of Alberta, Canada)</i><br>
+							 Dimitris Papadias <i>(Hong Kong University of Science and Technology, China)</i><br>
+							 Dieter Pfoser <i>(George Mason University, United States)</i><br>
+							 Matthias Renz <i>(Ludwig-Maximilians University Munich, Germany)</i>
+							<br><br>
+							
+							<h3>Program Co-Chairs</h3>
+							Benjamin Adams <i>(University of Auckland, New Zealand)</i><br>
+							Dingming Wu <i>(University of Hong Kong, Hong Kong)</i><br>
+							Andreas Z&uuml;fle <i>(George Mason University, Fairfax, USA)</i>
+							<br><br>
+						
+							<h3>Publicity Chair</h3>
+							Panagiotis Bouros <i>(Aarhus University, Denmark)</i>
+							<br><br>
+							
+							<h3>Publication and Proceedings Chair</h3>
+							Leong Hou U <i>(University of Macau, China)</i>
+							<br><br>
+							
+							<h3>Webmaster</h3>
+							Klaus Arthur Schmid <i>(Ludwig-Maximilians University Munich, Germany)</i>
+							<br><br>
+							
+							<h3>Programm Committee</h3>
+							Panagiotis Bouros <i>(Aarhus University, Denmark)</i><br/>
+							Muhammad Cheema <i>(Monash University, Australia)</i><br/>
+							Reynold Cheng <i>(The University of Hong Kong)</i><br/>
+							Mark Gahegan <i>(University of Auckland, Australia)</i><br/>
+							Michael Gertz <i>(Heidelberg University, Germany)</i><br/>
+							Gabriel Ghinita <i>(University of Massachusetts, Boston, USA)</i><br/>
+							Diansheng Guo <i>(University of South Carolina, Columbia, USA)</i><br/>
+							Ralf Hartmut Güting <i>(Fernuniversität Hagen,Germany)</i><br/>
+							Erik Hoel <i>(ESRI)</i><br/>
+							Krzysztof Janowicz <i>(University of California, Santa Barbara, USA)</i><br/>
+							Christian S. Jensen <i>(Aalborg University, Denmark)</i><br>
+							Christopher Jones <i>(Cardiff University, UK)</i><br/>
+							Tomi Kauppinen <i>(Aalto University School of Science, Finland)</i><br/>
+							Werner Kiessling <i>(University of Augsburg, Germany)</i><br/>
+							Werner Kuhn <i>(University of California, Santa Barbara, USA)</i><br/>
+							Hua Lu <i>(Aalborg University, Denmark)</i><br/>
+							Nikos Mamoulis <i>(The University of Hong Kong, China)</i><br>
+							Grant McKenzie <i>(University of California, Santa Barbara, USA)</i><br/>
+							Dimitris Papadias <i>(HKUST, Hong Kong)</i><br/>
+							Dieter Pfoser <i>(George Mason University, USA)</i><br/>
+							Ross Purves <i>(University of Zürich, Switzerland)</i><br/>
+							Dimitris Sacharidis <i>(Technische Universitaet Wien, Austria)</i><br/>
+							Sven Schade <i>(European Commission - DG Joint Research Centre)</i><br/>
+							Simon Scheider <i>(University Utrecht, The Netherlands)</i><br/>
+							Yufei Tao <i>(Chinese University of Hong Kong, Hong Kong)</i><br/>
+							Leong Hou U <i>(University of Macau, China)</i><br/>
+							Maria Vasardani <i>(The University of Melbourne, Australia)</i><br/>
+							Carola Wenk <i>(Tulane University, USA)</i><br/>
+							Stephan Winter <i>(The University of Melbourne, Australia)</i><br/>
+							Ouri Wolfson <i>(University of Illinois, USA)</i><br/>
+							Raymond Chi‐Wing Wong <i>(HKUST, Hong Kong)</i><br/>
+							Jianliang Xu <i>(Hong Kong Baptist University, Kowloon Tong, Hong Kong)</i><br/>
+							Man Lung Yiu <i>(Hong Kong Polytechnic University, Hong Kong)</i><br/>
+							Demetrios Zeinalipour‐Yazti <i>(University of Cyprus, Cyprus)</i><br/>
+							Xiaofang Zhou <i>(University of Queensland, Australia)</i><br/>
+						
+						<br class="clear" />
+					</div>
+					<br class="clear" />
+				</div>
+				<!--<div id="footer">
+					<div id="footerContent">
+										<div id="copyright">
+				&copy; GeoRich 2016 | Design: <a href="http://templated.org/free-css-templates/reverseobscurity/">Reverse Obscurity</a> by <a href="http://nodethirtythree.com">nodethirtythree</a> + <a href="http://templated.org/">Templated.org</a> <br/> Melbourne Image by <a href="http://www.sigmod2015.org">Sigmod</a>
+			</div>
+		</div>
+    </body>
+</html>
+					</div>
+					<br class="clear" />
+				</div>-->
+			</div>
+			<div id="copyright">
+				&copy; GeoRich 2016 | Design: <a href="http://templated.org/free-css-templates/reverseobscurity/">Reverse Obscurity</a> by <a href="http://nodethirtythree.com">nodethirtythree</a> + <a href="http://templated.org/">Templated.org</a> <br/> Melbourne Image by <a href="http://www.sigmod2015.org">Sigmod</a>
+			</div>
+		</div>
+    </body>
+</html>
